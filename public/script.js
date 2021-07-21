a1c6b1ab9b4f481ae7261c7b5f5d745a8322fd06
Lê Văn Thiện <52475801+epitchi@users.noreply.github.com>
2021-07-21 10:39:23 +0700

    Connect between peer stream video  and fix ui show video room.ejs

--- public/script.js
+++ public/script.js
@@ -13,15 +13,26 @@ navigator.mediaDevices.getUserMedia({
     audio: true
 }).then(stream => {
     addVideoStream(myVideo, stream)
-})
 
-myPeer.on('open', id => {
-    socket.emit('join-room', ROOM_ID, id)
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
 })
 
 socket.on('user-connected', userId => {
     console.log('User connected: ' + userId)
 })
+
+function connectToNewUser(userId, stream){
+    const call = myPeer.call(userId, stream)
+    const video = docuemnt.createElement('video')
+    call.on('stream', uservideoStream =>{
+        addVideoStream(video, uservideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+}
 
 function addVideoStream(video, stream) {
     video.srcObject = stream;
